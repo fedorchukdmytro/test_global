89b28b934ab057123d52acd160398b94c68d5c4a
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2021-12-13 20:06:12 +0200

    4

--- er.py
+++ er.py
@@ -1,17 +1,17 @@
-# import subprocess
+import subprocess
 import json
 
-# def run_server():
-#     server_process = subprocess.Popen(['iperf3', '-s', '-1'])
-#     # server_process.wait()
-# run_server()
+def run_server():
+    server_process = subprocess.Popen(['iperf3', '-s', '-1'])
+    # server_process.wait()
+run_server()
 
 
-# def run_client():  
-#     with open('output.json', 'w') as f:
-#         client_process = subprocess.Popen(['iperf3', '-c', '127.0.0.1', '-J'], stdout=f,) 
-#         client_process.wait()
-# run_client()
+def run_client():  
+    with open('output.json', 'w') as f:
+        client_process = subprocess.Popen(['iperf3', '-c', '127.0.0.1', '-J'], stdout=f,) 
+        client_process.wait()
+run_client()
 
 
 def test_iperf_speed():
