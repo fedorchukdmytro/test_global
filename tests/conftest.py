f45e874a72c6d4bdb51b51e3df0b2c0b0a3e6b52
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2021-12-14 23:57:51 +0200

    6

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,14 @@
-import pytest 
+import pytest
 import subprocess
+
 
 @pytest.fixture
 def run_server():
     server_process = subprocess.Popen(['iperf3', '-s', '-1'])
-    
+
+
 @pytest.fixture
-def run_client():  
-    with open('output.json', 'w') as f:
-        client_process = subprocess.Popen(['iperf3', '-c', '127.0.0.1', '-J'], stdout=f,) 
+def run_client():
+    with open('/Users/dmytrofedorchuk/glob/test_global/tests/output.json', 'w') as f:
+        client_process = subprocess.Popen(['iperf3', '-c', '127.0.0.1', '-J'], stdout=f,)
     client_process.wait()
-
-
-    
--- tests/test_iperf.py
+++ tests/test_iperf.py
@@ -1,9 +1,8 @@
 import json
 
-def test_iperf_speed(run_server,run_client):
-    with open('output.json', 'r') as data_file:
-        data = json.load(data_file)
-    assert (data['end']['streams'][0]['receiver']['bytes']/1000000) > 40
-    assert (data['end']['streams'][0]['receiver']['bits_per_second']/1000000) > 5
 
-    
+def test_iperf_speed(run_server, run_client):
+    with open('/Users/dmytrofedorchuk/glob/test_global/tests/output.json', 'r') as data_file:
+        data = json.load(data_file)
+    assert (data['end']['streams'][0]['receiver']['bytes'] / 1000000) > 40
+    assert (data['end']['streams'][0]['receiver']['bits_per_second'] / 1000000) > 5
